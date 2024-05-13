263697dd53ab2e8cbdc575b0bc8fb7db029becb1
Daniel Chin <DanChin327@gmail.com>
2024-05-12 19:40:38 -0600

    working on JS.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 const apiURL = "" //Write the URL Here.
+
+
+function translateToJapanese() {
+  const input = document.getquerySelector("#textToTranslate")
+  const japOutput =
+
+}
+
+function translateToJap() {}
+
+
+
+
+
+
+
+// The following is an example of using Dictionary
+const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+const displayDefinition = (event) => {
+  event.preventDefault();
+  const word = document.getElementById("word").value;
+  // TODO: Display your age with an AJAX call instead of the console.log()
+  fetch(apiUrl + word)
+    .then(response => response.json())
+    .then(data => {
+      const definition = data[0].meanings[0].definitions[0].definition;
+      const definitionElement = document.getElementById("definition");
+      definitionElement.innerHTML = definition;
+    })
+}
+
+const form = document.getElementById("fetch-word");
+form.addEventListener("submit", displayDefinition);
